6972e1a1757279c8eee3fe4f8117eb25e4aae78c
felipegs07 <felipegs07@gmail.com>
2018-05-28 17:01:10 -0300

    Translation working

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 let searchInput = document.querySelector('#textQuery');
 searchInput.addEventListener('keyup',() => {
     let searchText = document.querySelector('#textQuery').value;
-    
-    axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180528T163234Z.bba076e88d4c4ad9.3fdb9074e8add953be6bf4f2fa1983816fbf403d&lang=' + search + '&apikey=thewdb')
-    .then((response) => {
-        let translation = response;
-    })
-    .catch((err) => {
-        console.log(err);
-});
+    if(searchText != ''){
+        axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180528T163234Z.bba076e88d4c4ad9.3fdb9074e8add953be6bf4f2fa1983816fbf403d&lang=pt&text=' + searchText)
+        .then((response) => {
+            console.log(response);
+            //let translation = response;
+            let translateInput = document.querySelector('#translatedText');
+            translateInput.value = response.data.text[0];
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
 })
